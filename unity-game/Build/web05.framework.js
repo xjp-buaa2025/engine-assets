b913ee235a296add09af32a49af9ea5187889e1b
xjp-buaa2025 <xjpbuaaa2025@163.com>
2025-05-09 12:31:27 +0800

    添加发动机模型和Unity WebGL文件

---
+++ unity-game/Build/web05.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:934c4cc068e9bd8c890c81ceda17851b3b3292141164da7ec07a312741e654d2
+size 381433
---
+++ unity-game/Build/web05.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:05c7bc862735b7c3c88c066ddd69490fcc9ec3d0aa0d3e79a36c27a4562162c9
+size 19122
